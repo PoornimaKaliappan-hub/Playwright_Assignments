e847f040e17131d641df522232dffa80a286e1e1
PoornimaKaliappan-hub <poornimak130988@gmail.com>
2025-11-25 18:13:02 -0700

    Add files via upload

---
+++ datatypes_Nov 24_var.js
@@ -0,0 +1,20 @@
+var firstName='Poornima Kaliappan'
+console.log(firstName)
+console.log(typeof firstName)
+
+
+var companyName='Coforge'
+console.log(companyName)
+console.log(typeof companyName)
+
+var mobileNumber=9856709876
+console.log(mobileNumber)
+console.log(typeof mobileNumber)
+
+var isAutomation=true
+console.log(isAutomation)
+console.log(typeof isAutomation)
+
+var hasPlaywright
+console.log(hasPlaywright)
+console.log(typeof hasPlaywright)
---
+++ datatypes_Nov24-let.js
@@ -0,0 +1,20 @@
+let firstName='Poornima Kaliappan'
+console.log(firstName)
+console.log(typeof firstName)
+
+
+let companyName='Coforge'
+console.log(companyName)
+console.log(typeof companyName)
+
+let mobileNumber=9856709876
+console.log(mobileNumber)
+console.log(typeof mobileNumber)
+
+let isAutomation=true
+console.log(isAutomation)
+console.log(typeof isAutomation)
+
+let hasPlaywright
+console.log(hasPlaywright)
+console.log(typeof hasPlaywright)
