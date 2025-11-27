7cf183744adf875f81e6d2bf730abd96791f74cd
PoornimaKaliappan-hub <poornimak130988@gmail.com>
2025-11-27 11:40:00 -0700

    Add files via upload

---
+++ OddorEven.js
@@ -0,0 +1,15 @@
+function oddoreven()
+{
+    let a='15';
+    if(a%2==0)
+    {
+        console.log('The number given in even:',a);
+    }
+   
+    else
+        {
+        console.log('The number is odd:',a);
+    }
+    
+}
+oddoreven()
