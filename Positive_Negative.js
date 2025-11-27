f2f2400cf3a4f36cc28b81664547e40521bdb683
PoornimaKaliappan-hub <poornimak130988@gmail.com>
2025-11-27 11:52:08 -0700

    Add files via upload

---
+++ Positive_Negative.js
@@ -0,0 +1,17 @@
+function positiveOrNegative()
+{
+    let a=-0;
+    if(a>0)
+    {
+        console.log('The number is positive:',a);
+    }
+    else if(a<0)
+{
+        console.log('The number is negative:',a);
+}
+else 
+{
+    console.log('The number is zero:',a);
+}
+}
+positiveOrNegative()
